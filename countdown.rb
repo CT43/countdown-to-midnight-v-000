3c005ac41a85b2bbec23545fd95c73706f5847f1
Christian Thomas <christian.thomas.2894@gmail.com>
2018-06-21 20:15:28 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -6,5 +6,13 @@ def countdown (seconds)
     seconds -= 1
   end
    "HAPPY NEW YEAR!"
+end
 
+def countdown_with_sleep 
+  while seconds > 0
+    sleep(1)
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+   "HAPPY NEW YEAR!"
 end
